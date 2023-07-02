216a4641c8b3301f2a140196404749c86b343750
DanilZ <kraslvoe@mail.ru>
2023-07-03 02:05:34 +0500

    init repo

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { RouterView } from 'vue-router';
+import HelloWorld from './components/Home.vue';
+</script>
+
+<template>
+    <header>
+        <div class="wrapper">
+            <HelloWorld msg="You did it!"/>
+
+            <nav>
+                <RouterView></RouterView>
+            </nav>
+        </div>
+    </header>
+
+    <RouterView/>
+</template>
+
+<style scoped>
+
+</style>
