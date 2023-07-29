dfd35dcd9e9605cc9f9ec34a7f0ea207e3fe6ae0
DanilZ <kraslvoe@mail.ru>
2023-07-30 01:05:23 +0500

    ADD | add header markup and styles

--- src/components/Header/HeaderItem.vue
+++ src/components/Header/HeaderItem.vue
@@ -1,8 +1,32 @@
-<script setup=""></script>
+<script setup>
+import logoImg from '@/assets/images/headerLogo.svg';
+import { ref } from 'vue';
+
+const linksItems = ref(['ВОЗМОЖНОСТИ', 'ПРОЦЕСС', 'ПОДКЛЮЧИТЬСЯ']);
+</script>
 
 <template>
     <header class="header">
-        <div class="header__inner container">Это шапка</div>
+        <v-container class="header__inner">
+            <nav class="header__nav">
+                <div class="header__logo">
+                    <v-img :src="logoImg" height="32" width="176" />
+                </div>
+                <ul class="list">
+                    <li v-for="link in linksItems" :key="link" class="list__item">
+                        <a class="link" href="#gg">{{ link }}</a>
+                    </li>
+                    <li class="list__item">
+                        <v-btn
+                            :ripple="false"
+                            class="header__btn btn-outlined rounded-sm"
+                            elevation="0"
+                            >АВТОРИЗАЦИЯ
+                        </v-btn>
+                    </li>
+                </ul>
+            </nav>
+        </v-container>
     </header>
 </template>
 
